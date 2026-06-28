4a87b400bb8d2675070f32fd73c5ac1068851e76
AudricDev <atfatoky@gmail.com>
2026-06-28 18:23:26 +0300

    add new model

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from accounts.models import ProfilUtilisateur
+from VEnchere.models import Categorie,Enchere
 # Register your models here.
 class ProfilUtilisateurAdmin(admin.ModelAdmin):
     class Meta:
-        list_display = ('phone_number','role','avatar','user','date_creation')
+        list_display = ('id','phone_number','role','avatar','user')
 admin.site.register(ProfilUtilisateur,ProfilUtilisateurAdmin)
         
-        
+# affichage du categorie dans admin django
+class CategorieAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ('id','nom','slug')
+admin.site.register(Categorie,CategorieAdmin)
+
+# affichage de l'enchere dans admin
+class EnchereAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ('titre','description','prix_depart','prix_actuel','prix_reserve','date_debut','date_fin','statut','vendeur','categorie')
+admin.site.register(Enchere, EnchereAdmin)
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -32,64 +32,8 @@ class ProfilUtilisateur(models.Model):
         null=True
     )
 
-    date_creation = models.DateTimeField(auto_now_add=True, blank=True)
+    date_creation = models.DateTimeField(auto_now_add=True, blank=True,null=True)
 
     def __str__(self):
         return self.user.username
 
-# modele catégorie
-class Categorie(models.Model):
-
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False
-    )
-
-    nom = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.nom
-
-# modele enchère
-class Enchere(models.Model):
-
-    STATUT = (
-        ('ouverte','Ouverte'),
-        ('terminee','Terminée'),
-        ('annulee','Annulée'),
-    )
-
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False
-    )
-
-    titre = models.CharField(max_length=200)
-    description = models.TextField()
-    prix_depart = models.DecimalField(max_digits=12, decimal_places=2)
-    prix_actuel = models.DecimalField(max_digits=12, decimal_places=2)
-    prix_reserve = models.DecimalField(max_digits=12, decimal_places=2)
-    date_debut = models.DateTimeField()
-    date_fin = models.DateTimeField()
-    statut = models.CharField(
-        max_length=20,
-        choices=STATUT,
-        default='ouverte'
-    )
-
-    vendeur = models.ForeignKey(
-        ProfilUtilisateur,
-        on_delete=models.CASCADE,
-        related_name='encheres'
-    )
-
-    categorie = models.ForeignKey(
-        Categorie,
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.titre
