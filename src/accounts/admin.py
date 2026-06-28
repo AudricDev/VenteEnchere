1e8fce585de2868f34724444578cc6186ee38a26
AudricDev <atfatoky@gmail.com>
2026-06-28 09:29:38 +0300

    add admin django

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from accounts.models import ProfilUtilisateur
 # Register your models here.
+class ProfilUtilisateurAdmin(admin.ModelAdmin):
+    class Meta:
+        list_display = ('name','phone_number','role','avatar','user')
+admin.site.register(ProfilUtilisateur,ProfilUtilisateurAdmin)
+        
+        
