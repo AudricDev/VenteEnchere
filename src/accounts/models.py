eec2a6286e1c1dee8eb90eef46a38a5e33fde1a7
AudricDev <atfatoky@gmail.com>
2026-06-28 09:11:29 +0300

    add model accounts

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -9,6 +9,14 @@ class ProfilUtilisateur(models.Model):
     )
     name = models.CharField(max_length=50,blank=True)
     phone_number = models.CharField(max_length=50)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICE, default='Acheteur')
     avatar = models.ImageField(upload_to='Utilisateur', blank=True, null=True)
     
     user = models.OneToOneField(User,on_delete=models.CASCADE, blank=True,null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'ProfilUtilisateur'
+        verbose_name_plural = 'ProfilUtilisateurs'
