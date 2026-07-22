663188a11bbb36fe95ddfa9b285da8cceeee343d
AudricDev <atfatoky@gmail.com>
2026-07-22 21:09:26 +0300

    MaJ App

--- src/VEnchere/urls.py
+++ src/VEnchere/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('connexion/',views.connexion,name='connexion'),
+    path('inscription/',views.inscription,name='inscription'),
+    
 ]
--- src/VEnchere/views.py
+++ src/VEnchere/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout
 
 # Create your views here.
 def index(request):
     return HttpResponse("index here bro")
+
+def connexion(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = User.objects.filter(username = email, password = password).first()
+        login(request,user)
+        return render(request,'index.html')
+    return render(request,'connexion.html')
+
+def inscription(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        mdp1 = request.POST.get("mdp1")
+        mdp2 = request.POST.get("mdp2")
+        if mdp1 == mdp2:
+            data = User.objects.create_user(
+                username = email,
+                email = email,
+                password = mdp1
+            )
+            data.save()
+            login(request, data)
+            return render(request,'index.html')
+    return render(request,'inscription.html')
