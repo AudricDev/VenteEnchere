132609ba05e07ec580ccd58250c826fee74b0ad5
AudricDev <atfatoky@gmail.com>
2026-07-25 20:32:44 +0300

    MàJ

--- src/VEnchere/views.py
+++ src/VEnchere/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+#@login_required
 def index(request):
     return render(request, 'index.html')
 
